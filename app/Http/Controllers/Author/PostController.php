06449cf5e0c94ca46149f8b883d1646f690645f1
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-27 22:04:39 +0545

    Send Notification To Admin

--- app/Http/Controllers/Author/PostController.php
+++ app/Http/Controllers/Author/PostController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Author;
 
 use App\Category;
+use App\Notifications\NewAuthorPost;
 use App\Post;
 use App\Tag;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 use Brian2694\Toastr\Facades\Toastr;
@@ -87,6 +90,9 @@ class PostController extends Controller
 
         $post->categories()->attach($request->categories);
         $post->tags()->attach($request->tags);
+
+        $users = User::where('role_id', 1)->get();
+        Notification::send($users, new NewAuthorPost($post));
 
         Toastr::success('Post Successfully Saved :)','Success');
         return redirect()->route('author.post.index');
