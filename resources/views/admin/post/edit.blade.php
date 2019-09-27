014829fe38f96b3a24a9cfc6720d6e1f3bbcb9c2
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-27 16:15:36 +0545

    Edit Post

---
+++ resources/views/admin/post/edit.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.backend.app')
+
+@section('title', 'EDIT Post')
+
+@push('css')
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+
+        <form action="{{route('admin.post.update', $post->id)}}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row clearfix">
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                EDIT  POST
+                            </h2>
+                        </div>
+                        <div class="body">
+
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="text" id="title" name="title" class="form-control" value="{{$post->title}}">
+                                    <label class="form-label">Post Title</label>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="image">Featured Image</label>
+                                <input type="file" name="image" id="image" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="checkbox" id="publish" class="filled-in" name="status" value="1" {{ $post->status == true ? 'checked' : '' }}>
+                                <label for="publish">Publish</label>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Categories and Tags
+                            </h2>
+                        </div>
+                        <div class="body">
+
+                            <div class="form-group form-float">
+                                <div class="form-line {{ $errors->has('categories') ? 'focused error' : '' }}">
+                                    <label for="category">Select Category</label>
+                                    <select name="categories[]" id="category" class="form-control show-tick" data-live-search="true" multiple>
+                                        @foreach($categories as $category)
+                                            <option
+                                                    @foreach($post->categories as $postCategory)
+                                                    {{ $postCategory->id == $category->id ? 'selected' : '' }}
+                                                    @endforeach
+                                                    value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group form-float">
+                                <div class="form-line {{ $errors->has('tags') ? 'focused error' : '' }}">
+                                    <label for="tag">Select Tags</label>
+                                    <select name="tags[]" id="tag" class="form-control show-tick" data-live-search="true" multiple>
+                                        @foreach($tags as $tag)
+                                            <option
+                                                    @foreach($post->tags as $postTag)
+                                                    {{ $postTag->id == $tag->id ? 'selected' :'' }}
+                                                    @endforeach
+                                                    value="{{ $tag->id }}">{{ $tag->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+
+
+                            <a href="{{route('admin.post.index')}}" class="btn btn-danger m-t-15 waves-effect">BACK</a>
+                            <button type="submit" class="btn btn-primary m-t-15 waves-effect">UPDATE</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                BODY
+                            </h2>
+                        </div>
+                        <div class="body">
+                            <textarea name="body" id="tinymce">{{$post->body}}</textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </form>
+    </div>
+@endsection
+
+
+@push('js')
+
+
+
+@endpush
+
+
